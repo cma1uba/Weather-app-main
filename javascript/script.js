35e9adde0492e6d93685dd57ffbff72e9f6d4c34
Maluba Choonya <cmaluba8@gmail.com>
2025-10-06 21:23:25 +0200

    Update script.js
    Implemented functionality for drop-down menus

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,11 @@
 let subMenu = document.getElementById("subMenu");
+let dayDropdown = document.getElementById("dayDropdown");
 
+//Dropdown menu toggle
 function toggleMenu(){
     subMenu.classList.toggle("open-menu");
+}
+function toggleDropdown(){
+    dayDropdown.classList.toggle("open-menu");
 }
 
