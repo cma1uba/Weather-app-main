c2eff7feff75e1d5fc870c2ac034c3c112a166dc
Maluba Choonya <cmaluba8@gmail.com>
2025-10-07 19:33:52 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
 let subMenu = document.getElementById("subMenu");
-let dayDropdown = document.getElementById("dayDropdown");
-
-//Dropdown menu toggle
 
 function toggleMenu(){
     subMenu.classList.toggle("open-menu");
 }
+
+let dayDropdown = document.getElementById("dayDropdown");
+
 function toggleDropdown(){
     dayDropdown.classList.toggle("open-menu");
 }
